30aa89637fd5680bbbea2da82aa0170472720d70
Michael <notme@bieber-home.net>
2018-12-12 14:26:41 +0100

    Web Component bis auf data-list-icon

--- data-list-component.js
+++ data-list-component.js
@@ -1,15 +1,132 @@
-/* eslint-disable no-console */
-import { getItems } from "./item-service.js";
+// data-list-header
+const dataListHeaderTemplate = document.createElement("template");
+dataListHeaderTemplate.innerHTML = /*html*/`
+    <style>
+        :host {
+            text-transform: uppercase;
+            line-height: 1;
+            font-size: 16px;
+            padding: 5px 0 20px 10px;
+            margin: 10px 0 20px 0;
+            border-bottom: 1px solid black;
+            display: block;
+        }
+    </style>
+    <slot></slot>
+`;
 
-const dataToShow = getItems();
+class DataListHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(dataListHeaderTemplate.content.cloneNode(true));
+    }
+}
+window.customElements.define("data-list-header", DataListHeader);
 
-dataToShow.forEach(function(entry) {
-    for (let prop in entry) {
-        if(prop === "timestamp") {
-            entry[prop] = new Date(entry[prop]).toLocaleString();
-        } 
-        console.log(prop + ": " + entry[prop]);
-    }  
-});
+// data-list-content
+const dataListContentTemplate = document.createElement("template");
+dataListContentTemplate.innerHTML = /*html*/`
+    <style>
+        :host {
+            min-width: 100px;
+            flex-grow: 1;
+            margin-left: 10px;
+            padding-bottom: 20px;
+            border-bottom: 1px solid gray;
+            display: block;
+        }
+        ::slotted(h3) {
+            margin: 0;
+            padding: 0;
+            line-height: 1;
+            font-weight: normal;
+        }
+        ::slotted(p) {
+            margin: 0;
+            padding: 0;
+            font-size: 12px;
+            color: gray;
+        }
+    </style>
+    <slot></slot>
+`;
 
-//test
+class DataListContent extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(dataListContentTemplate.content.cloneNode(true));
+    }
+}
+window.customElements.define("data-list-content", DataListContent);
+
+//data-list-sequence
+const dataListSequenceTemplate = document.createElement("template");
+dataListSequenceTemplate.innerHTML = /*html*/`
+    <style>
+        :host {
+            color: gray;
+            font-size: 11px;
+            padding-right: 20px;
+            padding-bottom: 20px;
+            border-bottom: 1px solid gray;
+            display: block;
+        }        
+    </style>   
+    <slot></slot>
+`;
+
+class DataListSequence extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(dataListSequenceTemplate.content.cloneNode(true));
+    }
+}
+window.customElements.define("data-list-sequence", DataListSequence);
+
+// data-list-item
+const dataListItemTemplate = document.createElement("template");
+dataListItemTemplate.innerHTML = /*html*/`
+    <style>
+        :host {
+            display: flex;
+            flex-flow: row nowrap;
+            margin-left: 10px;
+            margin-bottom: 20px;
+        }        
+    </style>   
+    <slot></slot>
+`;
+
+class DataListItem extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(dataListItemTemplate.content.cloneNode(true));
+    }
+}
+window.customElements.define("data-list-item", DataListItem);
+
+// data-list
+const dataListTemplate = document.createElement("template");
+dataListTemplate.innerHTML = /*html*/`
+    <style>
+        :host {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin-bottom: 50px;
+            display: block;
+        }
+    </style>   
+    <slot></slot>
+`;
+
+class DataList extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.appendChild(dataListTemplate.content.cloneNode(true));
+    }
+}
+window.customElements.define("data-list", DataList);
